036af3ba7f3b41b21a3df19a26d617484ac299aa
Jo-Shaheen <yussifshaheen8@gmail.com>
2026-03-01 17:07:13 +0200

    The Class Diagrams added including the bad classes of vendor (still not finalized classes)
    The overall Main MVP design is done, just waiting to add react logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import "./styles/variables.css";
-import "./styles/globals.css";
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import BrowsePage from "./pages/BrowsePage";
+import Landingpage from "./pages/Landingpage";
+import TryOnHistoryPage from "./pages/TryOnHistoryPage";
+import DashboardPage from "./pages/DashboardPage";
 
 function App() {
-  return <HomePage />;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/browse" element={<BrowsePage />} />
+      <Route path="/landing" element={<Landingpage />} />
+      <Route path="/try-on-history" element={<TryOnHistoryPage />} />
+      <Route path="/dashboard" element={<DashboardPage />} />
+    </Routes>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import "./styles/variables.css";
 import "./styles/globals.css";
 import App from "./App.jsx";
-import Landingpage from "./pages/Landingpage.jsx";
-
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Landingpage />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 );
